dfa09080b74b96872e96bd76a52c1fab3074c807
mrgian <gianmatteo.palmieri@gmail.com>
2020-07-04 11:44:51 +0200

    Primo commit

---
+++ android/app/src/main/kotlin/it/mrgian/appquake/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.mrgian.appquake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AppQuake"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
