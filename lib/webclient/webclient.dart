a6901d1bd0114fc79c7da9d062869a07c1fd7a00
mrgian <mail@gian.im>
2021-03-11 22:48:43 +0100

    Fix encoding

--- lib/webclient/webclient.dart
+++ lib/webclient/webclient.dart
@@ -1,4 +1,5 @@
 import 'dart:convert' as convert;
+import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as html;
@@ -6,7 +7,7 @@ import 'package:html/parser.dart' as html;
 import 'package:appquake/model/terremoto.dart';
 
 class WebClient {
-  static const String baseUrl = 'https://gian.im/terremoti';
+  static const String baseUrl = 'https://api.terremoti.gian.im';
 
   static Future<List<Terremoto>> parseTerremoti(String responseBody) async {
     var parsed = convert.jsonDecode(responseBody).cast<Map<String, dynamic>>();
@@ -17,28 +18,29 @@ class WebClient {
     var response;
 
     try {
-      response = await http.get(baseUrl);
+      response = await http.get(Uri.parse(baseUrl));
     } catch (e) {
       return null;
     }
 
-    return parseTerremoti(response.body);
+    return parseTerremoti(utf8.decode(response.bodyBytes));
   }
 
   static Future<List<Map<String, dynamic>>> getStatistiche() async {
-    List<Map<String, dynamic>> stats = new List();
+    List<Map<String, dynamic>> stats = [];
 
     try {
       var response;
 
-      response = await http.get(baseUrl + '/stats');
-      stats.add(convert.jsonDecode(response.body));
+      response = await http.get(Uri.parse(baseUrl + '/stats'));
+      stats.add(convert.jsonDecode(utf8.decode(response.bodyBytes)));
 
-      response = await http.get(baseUrl + '/stats?field=valoreMagnitudo');
-      stats.add(convert.jsonDecode(response.body));
+      response =
+          await http.get(Uri.parse(baseUrl + '/stats?field=valoreMagnitudo'));
+      stats.add(convert.jsonDecode(utf8.decode(response.bodyBytes)));
 
-      response = await http.get(baseUrl + '/stats?field=profondita');
-      stats.add(convert.jsonDecode(response.body));
+      response = await http.get(Uri.parse(baseUrl + '/stats?field=profondita'));
+      stats.add(convert.jsonDecode(utf8.decode(response.bodyBytes)));
     } catch (e) {
       return null;
     }
@@ -47,7 +49,7 @@ class WebClient {
   }
 
   static Future<String> getMapLink(Terremoto terremoto) async {
-    var response = await http.get(terremoto.link);
+    var response = await http.get(Uri.parse(terremoto.link));
     var body = response.body;
     var link =
         html.parse(body).getElementsByTagName('iframe').first.attributes['src'];
